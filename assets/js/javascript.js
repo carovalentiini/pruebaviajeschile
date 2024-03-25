65fe5927869537b6ad8ea0961ac261b9596620d8
Carolina Valentini <c.valentini.a@gmail.com>
2024-03-25 13:26:43 -0300

    proyecto viajes chile 25 de marzo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,35 @@
+// Activar Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//Activar popover
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//alert
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Bien! tu correo ha sido enviado :)', 'success')
+  })
+}
+
+
 $(document).ready(function() {
     var a = $(".navbar").offset().top;
 
@@ -10,3 +42,4 @@ $(document).ready(function() {
         }
     });
 });
+
